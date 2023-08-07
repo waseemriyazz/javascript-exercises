dd9dfd4887cc6c1b003c3ce8b02e9c678fefaa64
waseemriyazz <waseemriyaz717@gmail.com>
2023-08-07 11:18:00 +0530

    solved 4-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1 , num2) {
+    if(num1>=0 && num2>=0 && typeof(num1)==typeof(72) &&typeof(num2)==typeof(72))
+    {
+        const larger = num1>num2? num1:num2;
+        let smaller = num1+num2-larger;
+        let sum=0;
+        for(;smaller<=larger;smaller++)
+        {
+            sum=sum+smaller;
+        }
+        return sum;
+    }
+    else
+    {
+        return 'ERROR';
+    }
 
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFarn) {
+  return Math.round((tempInFarn-32) * (5/9)*10)/10 ;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCel) {
+  return Math.round((tempInCel*9/5)*10)/10 + 32 ;
 };
 
 // Do not edit below this line
