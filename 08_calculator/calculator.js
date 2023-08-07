3f1589a29eefddc6cc412f076a6923ea9ff354b2
waseemriyazz <waseemriyaz717@gmail.com>
2023-08-07 15:06:10 +0530

    done with exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,51 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1 , num2) {
+  return num1 - num2;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  let sum = 0;
+  array.forEach(element => {
+    sum =sum+element;
+    
+  }); 
+  return sum;
 	
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let muliply=1;
+  array.forEach(element => {
+    muliply=muliply*element;
+  });
+  return muliply;
 };
 
-const power = function() {
+const power = function(num1, num2) {
+  let temp=1;
+  while (num2-->0)
+  {
+    temp=temp*num1;
+  }
+  return temp;
 	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if (num == 0)
+  {return 1}
+  let answer = 1;
+  while(num>0)
+  {
+    answer=num*answer;
+    num--;
+  }
+  return answer;
 	
 };
 
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let first  = 1;
+    let second = 2;
+    let answer = 0;
+    if(num<0)
+    {
+        return 'OOPS';
+    }
+    if(num==1 || num==2)
+    {
+        return 1;
+    }
+    for(let i=3;i<num;i++)
+    {
+        answer = first + second;
+        first=second;
+        second=answer;
+    }
+    return answer;
 
 };
 
